d20a8678431adb05cb993bc8714c81529249a60c
anzarsh <anzarsh@mail.ru>
2016-06-02 09:46:11 +0300

    lsdkj

--- wp-content/themes/olddog/index.php
+++ wp-content/themes/olddog/index.php
@@ -2,186 +2,14 @@
         
         <section class="content">
             <div class="cent">
-                
-                <div class="left_column">
-                    <div class="catalog box-shadow">
-                        <h3>Каталог товаров</h3>
-                        <ul id="menu"> 
-                            <li><a href="#"><span>Удилища</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Катушки</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Приманки</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Леска</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Аксессуары</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Электроника</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Одежда, обувь</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Оснастка</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Туризм</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#"><span>Лодки, моторы</span></a>
-                                <ul>
-                                    <li><a href="#">воблеры</a></li>
-                                    <li><a href="#">вращающиеся блесна</a></li>
-                                    <li><a href="#">колеблющиеся блесна</a></li>
-                                    <li><a href="#">карповые</a></li>
-                                    <li><a href="#">зимние блесны</a></li>
-                                    <li><a href="#">наживки</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    
-                    <div class="subscription box-shadow">
-                        <form action="#">
-                            <h5>Актуальная информация о скидках, акциях, новинках <span>Подпишитесь!</span></h5>
-                            <input type="text" class="inp_sub" value="" placeholder="Ваш e-mail">
-                            <button class="btn green">Подписаться</button>
-                        </form>
-                    </div>
-                    
-                    <div class="widget"><img src="<?php echo get_template_directory_uri(); ?>/images/vk.jpg" alt=""></div>
-                    
-                    <div class="news">
-                        <h3><a href="#">новости</a></h3>
-                        <ul>
-                            <?php
-                            wp_reset_query(); 
-                                
-                            $wp_query = new WP_Query(array(
-                               'cat' => '7'
-                                ));
-                             
-                                while ($wp_query->have_posts()) : $wp_query->the_post();
-                            ?>
-                            <li>
-                                <span><?php the_time('d.m.Y'); ?></span>
-                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                            </li>
-                            
-                         <?php endwhile;?>
-
-                            <li><span>19.06.2014</span> <a href="#">20% скидка на синнинги "Норстрим"</a></li>
-                            <li><span>12.04.2014</span> <a href="#">Расширен ассортимент Воблеры Major Craft и Kosadaka</a></li>
-                            <li><span>21.02.2014</span> <a href="#">На склад поступила продукция торговой марки Сабанеев + полный ассортимент кормушек FeederSport</a></li>
-                        </ul>
-                    </div>
-                    
-                    <div class="block_text">
-                        <h3>Рыболовный магазин DomRibaka.net - рыболовные снасти и товары для рыбалки.</h3>
-                        <p>Огромный ассортимент рыболовных товаров, которые представлены в нашем интернет-магазине.</p>
-                        <p>Недорогой и суперпопулярный спиннинг Shimano Catana или эксклюзивный, сверхлегкий в своем классе <a href="#">Daiwa BRANZINO</a>... Или может спиннинг St.Croix Avid, который завоевал сердца любителей джиговой ловли?</p>
-                    </div>
-                    
-                </div><!-- left_column -->
+            <?php get_sidebar(); ?>
                 
                 <div class="content_frame clearfix">
                     <div class="w100">
                         
-                        <div class="search">
-                            <form action="#">
-                                <input type="text" class="inp_s" value="" placeholder="Поиск по сайту">
-                                <button class="btn green">Найти</button>
-                            </form>
-                        </div>
-                        
-                        <div class="promotion clearfix">
-        
-                            <div class="left">
-                                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider1.jpg" alt=""></a>
-                            </div>
-                            <div class="right">
-                                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider2.jpg" alt=""></a>
-                            </div>
 
-                        </div>
-                        
-                        <div class="info clearfix">
-                            <ul>
-                                <li><span><i class="icon ic1"></i>Бесплатная доставка</span></li>
-                                <li><span><i class="icon ic2"></i>Официальные поставки</span></li>
-                                <li><span><i class="icon ic3"></i>Как купить в магазине</span></li>
-                                <li><span><i class="icon ic4"></i>Скидки всем покупателям</span></li>
-                            </ul>
-                        </div>
+                        <?php require_once('search_form.php'); ?>
+                        <?php require_once('promo.php'); ?>
                         
                         <div class="categories">
                             <ul>
@@ -202,90 +30,79 @@
                             <h3>Производители</h3>
                             <a href="#" class="btn">Все производители</a>
                             <div class="owl-carousel">
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand1.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand2.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand3.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand4.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand1.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand2.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand3.jpg" alt=""></div>
-                                <div><img src="<?php echo get_template_directory_uri(); ?>/images/brand4.jpg" alt=""></div>
+                                <?php wp_reset_query(); ?>
+                                <?php $wp_query = new WP_Query(array(
+                                       'cat' => '9'
+                                        )); ?>
+                                <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+                                <div>
+                                    <?php the_post_thumbnail(); ?>
+                                </div>
+                                <?php endwhile;?>
                             </div>
                         </div>
                         
                         <div class="video_reviews">
                             <h3><a href="#">Видеообзоры</a></h3>
                             <ul>
+                                <?php wp_reset_query(); ?>
+                                <?php $wp_query = new WP_Query(array(
+                                       'cat' => '10'
+                                        )); ?>
+                                <?php while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
                                 <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/video1.jpg" alt="">
-                                    <a href="#">Хотел бы я быть здесь / Wish I Was Here (Обзор)</a>
+                                    <?php the_post_thumbnail(); ?>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <?php the_title(); ?>
+                                    </a>
                                 </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/video2.jpg" alt="">
-                                    <a href="#">РБК: эксперт раскритиковал доклад минобороны РФ по крушению Boeing-777</a>
-                                </li>
-                                <li>
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/video3.jpg" alt="">
-                                    <a href="#">Мультик Фиксики - Все серии подряд - Сборник 13</a>
-                                </li>
+                                <?php endwhile;?>
                             </ul>
                         </div>
                         
                         <div class="articles">
                             <h3><a href="#">Статьи и информация</a></h3>
-                                <?php 
-                                wp_reset_query(); 
-                                $wp_query = new WP_Query(array(
+                            <?php
+                                 wp_reset_query();
+                                 $wp_query = new WP_Query(array(
                                    'cat' => '8'
                                     ));
                                  
                                     while ($wp_query->have_posts()) : $wp_query->the_post();
-                                    if (get_field('на_главную')=='да'){
+
                                 ?>
+                            <?php if(get_field('на_главной') == 'да'){ ?>    
                             <div class="art_large">
-                                <img src="<?php echo get_template_directory_uri(); ?>/images/article1.jpg" alt="">
-                                <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                                <?php the_content(); ?>
-                                <?php } ?>
+                            
+                                <?php the_post_thumbnail(); ?>
+                                <h4><a href="<?php the_permalink(); ?>">
+                                    <?php the_title(); ?>
+                                </a></h4>
+                                <?php the_content(); ?> 
+                            
+                            
                             </div>
+                            <?php } ?>
                             <?php endwhile;?>
+                            
                             <div class="art_small">
 
-                                <?php 
-                                wp_reset_query(); 
-                                $wp_query = new WP_Query(array(
+                                <?php
+                                 wp_reset_query();
+                                 $wp_query = new WP_Query(array(
                                    'cat' => '8'
                                     ));
                                  
                                     while ($wp_query->have_posts()) : $wp_query->the_post();
                                 ?>
-                                
+                                <?php if(get_field('на_главной') != 'да'){ ?>
                                 <div class="item">
                                     <?php the_post_thumbnail(); ?>
                                     <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-                                    <p><?php the_content(); ?> </p>
+                                    <?php the_content(); ?>
                                 </div>
+                                <?php } ?>
                                 <?php endwhile;?>
-                                <div class="item">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/article2.jpg" alt="">
-                                    <h4><a href="#">Искусственная приманка для сома</a></h4>
-                                    <p>Вы знаете рыболовные пруды, где водится круп...</p>
-                                </div>
-                                <div class="item">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/article3.jpg" alt="">
-                                    <h4><a href="#">Ловим только крупного карася</a></h4>
-                                    <p>Замечали ли вы когда-либо, каким мощным представлен лоб карася...</p>
-                                </div>
-                                <div class="item">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/article4.jpg" alt="">
-                                    <h4><a href="#">Обувь для рыбака</a></h4>
-                                    <p>Замечали ли вы когда-либо, каким мощным представлен лоб карася...</p>
-                                </div>
-                                <div class="item">
-                                    <img src="<?php echo get_template_directory_uri(); ?>/images/article5.jpg" alt="">
-                                    <h4><a href="#">Финляндия – мечта рыбака</a></h4>
-                                    <p>Замечали ли вы когда-либо, каким мощным представлен лоб карася...</p>
-                                </div>
                             </div>  
                         </div>
                         
---
+++ wp-content/themes/olddog/promo.php
@@ -0,0 +1,19 @@
+                        <div class="promotion clearfix">
+        
+                            <div class="left">
+                                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider1.jpg" alt=""></a>
+                            </div>
+                            <div class="right">
+                                <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider2.jpg" alt=""></a>
+                            </div>
+
+                        </div>
+                        
+                        <div class="info clearfix">
+                            <ul>
+                                <li><span><i class="icon ic1"></i>Бесплатная доставка</span></li>
+                                <li><span><i class="icon ic2"></i>Официальные поставки</span></li>
+                                <li><span><i class="icon ic3"></i>Как купить в магазине</span></li>
+                                <li><span><i class="icon ic4"></i>Скидки всем покупателям</span></li>
+                            </ul>
+                        </div>
